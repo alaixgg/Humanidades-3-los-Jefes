6643b130f5e5e6b42aaaa512b066ed34f3feae38
alaixgg <andresj.alaixp@utadeo.edu.co>
2025-03-26 04:39:22 -0500

    resumen

--- app/src/main/java/com/example/losjefeshumanidades3/cap23Activity.kt
+++ app/src/main/java/com/example/losjefeshumanidades3/cap23Activity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Cap1Activity : AppCompatActivity() {
+class cap23Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
